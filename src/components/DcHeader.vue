68e39da3cfdb0606545825b2b7e1c682780cc1f7
bigeppo <carlog91cast@gmail.com>
2022-07-15 15:46:24 +0200

    add imgs on asset

---
+++ src/components/DcHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header-container">
+        <div class="logo">
+            <img src="../" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
