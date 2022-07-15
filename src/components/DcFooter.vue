8001404ae785b5ed3e71d70b637043de27e02774
bigeppo <carlog91cast@gmail.com>
2022-07-15 19:07:51 +0200

    setting footer

---
+++ src/components/DcFooter.vue
@@ -0,0 +1,72 @@
+<template>
+    <footer>
+        <section class="list-menu">
+            <div class="lists">
+                <div class="list-left">
+                    <h3>dc comics</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <h3>shop</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="list-center">
+                    <h3>dc</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="list-right">
+                    <h3>sites</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'DcMain',
+    data() {
+        return {
+            iconsFooter: [
+
+            ]
+        }
+    }
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+@import '../style/common.scss';
+@import '../style/variables.scss';
+</style>
