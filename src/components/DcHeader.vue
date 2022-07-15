935293a0b074b8e22ef040b975e3638b3255ced6
bigeppo <carlog91cast@gmail.com>
2022-07-15 15:58:38 +0200

    add array to nav (commented)

--- src/components/DcHeader.vue
+++ src/components/DcHeader.vue
@@ -2,14 +2,73 @@
     <div class="header-container">
         <div class="logo">
             <img src="../assets/img/dc-logo.png" alt="">
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
         </div>
     </div>
 </template>
 
 <script>
+// export default {
+//     name: 'PageHeader',
+//     data() {
+//         return {
+//             navItems: [
+//                 {
+//                     text: 'CHARACTERS',
+//                     url: '#',
+//                 },
+//                 {
+//                     text: 'COMICS',
+//                     url: '#',
+//                 },
+//                 {
+//                     text: 'MOVIES',
+//                     url: '#',
+//                 },
+//                 {
+//                     text: 'TV',
+//                     url: '#',
+//                 },
+//                 {
+//                     text: 'GAMES',
+//                     url: '#',
+
+//                 },
+//                 {
+//                     text: 'COLLECTIBLES',
+//                     url: '#',
+
+//                 },
+//                 {
+//                     text: 'VIDEOS',
+//                     url: '#',
+
+//                 },
+//                 {
+//                     text: 'FANS',
+//                     url: '#',
+
+//                 },
+//                 {
+//                     text: 'NEWS',
+//                     url: '#',
+
+//                 },
+//                 {
+//                     text: 'SHOP',
+//                     url: '#',
+
+//                 }
+//             ],
+//         };
+//     }
+// }
 
 </script>
 
 <style>
-
 </style>
