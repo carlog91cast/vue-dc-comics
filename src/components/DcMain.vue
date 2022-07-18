8f52784274ee2b8acf001abd97664298eb432621
bigeppo <carlog91cast@gmail.com>
2022-07-18 15:04:24 +0200

    add img on dc main

--- src/components/DcMain.vue
+++ src/components/DcMain.vue
@@ -6,29 +6,17 @@
             </div>
         </section>
 
-        <section class="menu-icon">
-            <ul class="list-icon">
-                <li>
-                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                    <p>digital comics</p>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                    <p>merchandise</p>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                    <p>shop locator</p>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                    <p>subscriptions</p>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                    <p>dc power visa</p>
-                </li>
-            </ul>
+        <section class="product-container">
+            <div id="wrapper">
+                <div v-for="(product, index) in products" :key="index" class="product">
+                    <div class="img-container">
+                        <img :src="require(`../assets/img/${product.imgUrl}`)" :alt="product.text">
+                        <h3>
+                            {{ product.text }}
+                        </h3>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 </template>
@@ -38,8 +26,27 @@ export default {
     name: 'DcMain',
     data() {
         return {
-            icons: [
-
+            products: [
+                {
+                    text: 'Digital Comics',
+                    imgUrl: 'buy-comics-digital-comics.png'
+                },
+                {
+                    text: 'DC Merchandise',
+                    imgUrl: 'buy-comics-merchandise.png'
+                },
+                {
+                    text: 'Subscription',
+                    imgUrl: 'buy-comics-subscriptions.png'
+                },
+                {
+                    text: 'Shop Locatin',
+                    imgUrl: 'buy-comics-shop-locator.png'
+                },
+                {
+                    text: 'Dc Power Visa',
+                    imgUrl: 'buy-dc-power-visa.svg'
+                },
             ]
         }
     }
@@ -62,38 +69,34 @@ export default {
     font-size: 40px;
 }
 
-.menu-icon {
+section.product-container {
     background-color: $brand_color;
-    width: 100%;
-    height: 15vh;
 
-    .list-icon {
-        width: 70%;
-        margin: 0 auto;
+    div.wrapper {
+        padding: 2rem 2rem;
         display: flex;
 
-        li {
-            width: calc(100% / 5);
+        div.product {
             display: flex;
-            align-items: center;
-            margin-top: 30px;
-            
+            margin-right: 5rem;
 
-            img {
-                width: 20%;
-                height: 50px;
-
+            div.img-container img {
+                height: 5rem;
+                vertical-align: middle;
+                margin-right: 1rem;
             }
 
-            p {
+            h3 {
                 color: white;
                 font-size: 12px;
                 text-transform: uppercase;
                 margin-top: 10px;
                 margin-left: 10px;
-                
+
             }
         }
     }
+
+
 }
 </style>
